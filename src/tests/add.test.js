daa53932da3aeb0eefe7e61ddb1e0db9f7e1366e
BunColak <bunyamincolak@gmail.com>
2018-01-31 10:59:36 +0300

    Configured Jest and added a dummy test.

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,8 +1,16 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name) => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(3, 4);
-  if (result !== 7) {
-    throw new Error(`The result should be 7 it is not. Result: ${result}`)
-  }
+  // if (result !== 7) {
+  //   throw new Error(`The result should be 7 it is not. Result: ${result}`)
+  // }
+  expect(result).toBe(7);
+});
+
+test ('should contain name', () => {
+  const name = 'Bünyamin';
+
+  expect(generateGreeting(name)).toBe(`Hello Bünyamin!`)
 });
